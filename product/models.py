700ccd69e9f4bc8b8bba2513ced99c8bf963ca2c
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-05 23:37:11 +0900

    [ADD] seniorTest clone for ec2

---
+++ product/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=70)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Style(models.Model):
+    requestId = models.CharField(max_length=50, null=False)
+    tagName = models.CharField(max_length=50, null=False)
+    imgUrl = models.CharField(max_length=256, null=False)
+    img = models.BinaryField(null=False)
+
+    class Meta:
+        db_table = 'style'
+
+
+class Emoji(models.Model):
+    requestId = models.CharField(max_length=50, null=False)
+    tagName = models.CharField(max_length=50, null=False)
+    emojiTag = models.CharField(max_length=50, null=False)
+    emojiUrl = models.CharField(max_length=256, null=False)
+    emoji = models.BinaryField(null=False)
+    setNum = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = "emoji"
