6ac7308f373d2be62b6caa52f824a7e4f74fb140
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-22 07:14:09 +0900

    [UPDATE]

--- modelImport/settings.py
+++ modelImport/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    'django.middleware.http.ConditionalGetMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
